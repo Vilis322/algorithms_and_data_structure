a9bc40fee741d9bd9535ea4afd551d4cb5e89fd2
Vilis322 <viliskrill322@gmail.com>
2025-03-03 16:37:39 +0200

    circular queue

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -9,8 +9,8 @@ class CircularQueue:
             capacity (int): represents maximum capacity of the circular queue.
 
         Notes:
-            'self.front' (int): index of the first element. If is -1, the circular queue is empty.
-            'self.rear' (int): index of the last added element. If is (4+1) % 5 = 0 than the circular queue is full.
+            'self.front' (int): index of the first element.
+            'self.rear' (int): index of the last added element.
         """
         self.capacity = capacity
         self.queue = [None] * capacity
