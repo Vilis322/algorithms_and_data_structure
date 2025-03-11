dfd478dd4d23a0487284f26752c5826494cbc314
Vilis322 <viliskrill322@gmail.com>
2025-03-11 22:46:34 +0200

    added docstring

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -1,5 +1,41 @@
 def quick_sort(data: list, low: int = 0, high: int = None) -> list:
-    """Быстрая сортировка in-place."""
+    """Represents a quick sort algorithm.
+
+    The function sorts data by using a median of three elements: the first, middle, and last element.
+    It then creates two indices, `left` and `right`. The `left` index represents the position of
+    an element greater than or equal to the pivot, and the `right` index represents an element
+    less than the pivot. The function then enters a loop:
+    - The `left` index increments until an element greater than or equal to the pivot is found.
+    - The `right` index decrements until an element less than the pivot is found.
+    If `left` is less than or equal to `right`, the elements at `left` and `right` are swapped.
+    After partitioning, the function recursively sorts the left and right halves of the array.
+    The time complexity is O(n log n) on averagely, and O(n²) in the worst case.
+
+    Args:
+        data (list): The data to be sorted.
+        low (int) = The index of the first element in the data.
+        high (int) = The index of the last element in the data. In first calling it is 'None'.
+
+    Returns:
+        list: The sorted list.
+
+        Example:
+        Initial array: [5, 2, 9, 1, 5]
+
+        1. First, we pick the pivot using the median of three: [5, 9, 5] → Pivot = 5
+        2. Partitioning step: [2, 1, 5, 9, 5] (After first swap)
+        3. Left subarray: [2, 1, 5], Right subarray: [9, 5]
+
+        Left recursion:
+        4. Pivot for left subarray: [2, 1, 5] → Pivot = 2
+        5. Partitioning left: [1, 2, 5]
+
+        Right recursion:
+        6. Pivot for right subarray: [9, 5] → Pivot = 5
+        7. Partitioning right: [5, 9]
+
+        Final sorted array: [1, 2, 5, 5, 9]
+    """
     if high is None:
         high = len(data) - 1
 
